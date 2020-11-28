ab67ad5ebb4f3893996600d380840a4ba0cf6b59
Jacob McClure <jacobmcclure801@gmail.com>
2020-11-27 18:37:53 -0700

    commit on master

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 let playCount = 0;
+let userFirstGuess = "1st";
+let userSecondGuess = "2nd";
+let clickID = "";
+let guessToggle = false;
 
 //assigns variable to all image divs
 let imageChooser = document.querySelectorAll(".memoryDiv");
@@ -9,19 +13,41 @@ for (i = 0; i < imageChooser.length; i++) {
 }
 //function to display image
 function displayImage(event) {
-    let x = event.target;
-    let y = x.childNodes[1];
-    y.style.visibility = "visible";
+    let clickParent = event.target;
+    let clickItems = clickParent.childNodes;
+    let clickImg = clickItems[1];
+    //Name of image to match guesses
+    let clickName = clickItems[3].innerHTML;
+    clickImg.style.visibility = "visible";
+    
+    if (guessToggle == false) {
+        userFirstGuess = clickName;
+        guessToggle = true;
+        playCount++;
+    } else {
+        userSecondGuess = clickName;
+        guessToggle = false;
+        playCount++;
+    }
+    if (userFirstGuess == userSecondGuess) {
+        setTimeout(function(){ alert("You win!"); }, 500);
+    } else {
+        
+    }
+    console.log(userFirstGuess + " " + userSecondGuess);
     
     if (playCount >= 2) {
+        setTimeout(function() {
         allImageState[0].style.visibility = "hidden";
         allImageState[1].style.visibility = "hidden";
         allImageState[2].style.visibility = "hidden";
         allImageState[3].style.visibility = "hidden";
         allImageState[4].style.visibility = "hidden";
         allImageState[5].style.visibility = "hidden";
-        playCount = 0;
+        playCount = 0;}, 600);
+        userSecondGuess = "1st";
+        userSecondGuess = "2nd";
     } else {
-        playCount++
+        
     }
 }
